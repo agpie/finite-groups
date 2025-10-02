63579fcb2ec45f2557e577763a2ae2060ed7aa4b
Maxwell Johnson <maxwelljohnson144@gmail.com>
2025-10-02 16:42:35 +0100

    Add cyclic group generator and group property functions

--- src/finite_group.py
+++ src/finite_group.py
@@ -1,16 +1,14 @@
 import pandas as pd
 from itertools import product
 
-def create_z4():
+def create_Cn(n):
     ''' Create the group Z4 with its operation table '''
-    elements = [0, 1, 2, 3]
+    elements = list(range(n))
     table = {}
 
     pairs = list(product(elements, repeat=2))
-
     for (a, b) in pairs:
-        table[(a, b)] = (a + b) % 4
-
+        table[(a, b)] = (a + b) % n
     return elements, table
 
 def operate(a, b, table):
@@ -24,9 +22,41 @@ def display_table(table):
         df.loc[a, b] = result
     print(df)
 
+def find_identity(elements, table):
+    ''' Find the identity element in the group '''
+    for e in elements:
+        if all(operate(e, x, table) == x for x in elements) and all(operate(x, e, table) == x for x in elements):
+            return e
+    return None
+
+def check_order(elements, table):
+    ''' Check the order of each element in the group '''
+    e = find_identity(elements, table)
+    orders = {}
+    for g in elements:
+        order = 1
+        result = g
+        while result != e:
+            result = operate(result, g, table)
+            order += 1
+        orders[g] = order
+    return orders
+
+def is_abelian(elements, table):
+    ''' Check if the group is abelian '''
+    for i, g in enumerate(elements):
+        for j in range(i+1, len(elements)):
+            h = elements[j]
+            if table[(g, h)] != table[(h, g)]:
+                return False
+    return True
+
 if __name__ == "__main__":
-    elements, table = create_z4()
-    print("Elements of Z4:", elements)
+    elements, table = create_Cn(6)
+    print("Elements of Cn:", elements)
+    print("Order of each element:", check_order(elements, table))
+    print("Identity Element:", find_identity(elements, table))
+    print("Is the group abelian?", is_abelian(elements, table))
     print("Operation Table:")
     display_table(table)
 
