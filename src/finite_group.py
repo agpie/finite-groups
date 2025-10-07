acdfa6ea67d2ea0f6d25100287d599451bde6501
Maxwell Johnson <maxwelljohnson144@gmail.com>
2025-10-07 16:27:28 +0100

    Add dihedral group generator

--- src/finite_group.py
+++ src/finite_group.py
@@ -66,9 +66,28 @@ def create_Cn(n):
         table[(a, b)] = (a + b) % n
     return elements, table
 
+def create_Dn(n):
+    ''' Create the dihedral group Dn with its operation table '''
+    elements = list(range(n))
+    table = {}
+    labels = ['e'] + [f'r{i}' for i in range(1, n//2)] + ['s'] + [f'sr{i}' for i in range(1, n//2)]
+
+    for a in elements:
+        for b in elements:
+            if a < n//2 and b < n//2:  # r^i * r^j = r^(i+j)
+                table[(a, b)] = (a + b) % (n//2)
+            elif a < n//2 and b >= n//2:  # r^i * sr^j = sr^(j - i)
+                table[(a, b)] = n//2 + (b - a) % (n//2)
+            elif a >= n//2 and b < n//2:  # sr^i * r^j = sr^(i + j)
+                table[(a, b)] = n//2 + (a + b) % (n//2)
+            else:  # sr^i * sr^j = r^(j - i)
+                table[(a, b)] = (b - a) % (n//2)
+    
+    return elements, table, labels
+
 if __name__ == "__main__":
     
-    C4 = FiniteGroup(*create_Cn(4), labels=['e', 'a', 'a2', 'a3'])
+    C4 = FiniteGroup(*create_Cn(4), labels=['e', 'g', 'g2', 'g3'])
 
     print("Elements of C4:", C4.elements)
     print("Order of the group C4:", C4.order)
@@ -77,3 +96,13 @@ if __name__ == "__main__":
     print("Order of each element in C4:", {C4.get_label(k): v for k, v in C4.check_order().items()})
     print("Identity Element of C4:", C4.get_label(C4.identity))
     print("Is C4 abelian?", C4.is_abelian())
+
+    D6 = FiniteGroup(*create_Dn(6))
+
+    print("Elements of D6:", D6.elements)
+    print("Order of the group D6:", D6.order)
+    print("Operation Table of D6:")
+    D6.display_table()
+    print("Order of each element in D6:", {D6.get_label(k): v for k, v in D6.check_order().items()})
+    print("Identity Element of D6:", D6.get_label(D6.identity))
+    print("Is D6 abelian?", D6.is_abelian())
